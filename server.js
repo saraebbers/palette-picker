715593412df9cc61893ae496c7c9d40661cb0b87
Sara Ebbers <johndoe@example.com>
2019-01-22 15:54:22 -0700

    Add app.locals.projects or mock data

--- server.js
+++ server.js
@@ -7,6 +7,50 @@ app.set('port', process.env.PORT || 3000)
 // if this was running on production, there would be an environment port set already, this says, use that or if that isn't set/undefined, use port 3000
 app.locals.title = 'Palette Picker';
 
+app.locals.projects = [
+  {
+    id: 1,
+    name: 'Spring',
+    palettes: [
+      {
+        id: 11,
+        name: 'beach',
+        colors: [
+          '#E9D758', '#297373', '#FF8552', '#E6E6E6', '#39393A'
+        ]
+      },
+      {
+        id: 11,
+        name: 'ooh its green',
+        colors: [
+          '#ACECA1', '#96BE8C', '#629460', '#E6E6E6', '#39393A'
+        ]
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Fall',
+    palettes: [
+      {
+        id: 11,
+        name: 'Miami',
+        colors: [
+          '#F1E8B8', '#F9E784', '#E58F65', '#DO5353', '#191919'
+        ]
+      },
+      {
+        id: 11,
+        name: 'Sea Side',
+        colors: [
+          '#157F1F', '#4CB963', '#AOEADE', '#5C6784', '#1D263B'
+        ]
+      }
+      ]
+  }
+]
+//storage of data in a variable given to us by express.  Populated with fake data
+
 app.get('/', (request, response) => {
   response.send('Checking if it is connected correctly')
 });
