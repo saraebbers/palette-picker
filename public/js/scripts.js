38effdb4afe422abfe267116cbfdb01110794986
Sara Ebbers <johndoe@example.com>
2019-01-22 21:48:28 -0700

    Target nav buttons with callback functions

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,12 @@
+$(".save-btn").on('click', () => {
+  console.log('save-btn clicked')
+})
+
+$(".change-btn").on('click', () => {
+  console.log('change-btn clicked')
+})
+
+$(".retrieve-btn").on('click', () => {
+  console.log('retrieve-btn clicked')
+})
 
